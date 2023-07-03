75dae3ee56df54f8413ae7fbaff48658d9ab7c88
FilipePrata <eusouf.prata@gmail.com>
2023-07-03 17:41:05 -0300

    Listing Products

--- index.js
+++ index.js
@@ -9,14 +9,14 @@ class Product {
         if (this.validate(produto) == true) {
             this.save(produto)
         }
-        console.log(produto)
+        this.list()
     }
 
     readData() {
         let produto = {}
 
         produto.id = this.id;
-        produto.nome = document.getElementById("productName").value
+        produto.name = document.getElementById("productName").value
         produto.price = document.getElementById("productPrice").value
 
         return produto
@@ -25,7 +25,7 @@ class Product {
     validate(produto) {
         let msg = "";
 
-        if (produto.nome == "") {
+        if (produto.name == "") {
             msg += "Nome Inválido \n"
         }
 
@@ -44,6 +44,28 @@ class Product {
     save(produto) {
         this.arrayProducts.push(produto)
         this.id++
+    }
+
+    list() {
+        let tbody = document.getElementById("tbody")
+        tbody.innerText = ''
+
+        for (let i = 0; i < this.arrayProducts.length; i++) {
+
+            let tr = tbody.insertRow();
+
+            let td_id = tr.insertCell();
+            let td_name = tr.insertCell();
+            let td_price = tr.insertCell();
+            let td_del = tr.insertCell();
+
+            td_id.innerText = this.arrayProducts[i].id;
+            td_name.innerText = this.arrayProducts[i].name; 
+            td_price.innerText = this.arrayProducts[i].price;
+            let imagem = document.createElement("img")
+            imagem.src = "./img/trash-solid.svg"
+            td_del.appendChild(imagem)
+        }
     }
 }
 
